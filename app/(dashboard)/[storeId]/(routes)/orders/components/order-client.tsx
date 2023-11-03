edb071149590674484fae2141a999019de414bfc
Mahmodul Hasan <nicesanto92@gmail.com>
2023-11-03 17:47:26 +0600

    Order Page completed

--- app/(dashboard)/[storeId]/(routes)/orders/components/order-client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/order-client.tsx
@@ -1,41 +1,27 @@
 "use client";
 
 import Heading from "@/components/ui/Heading";
-import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Plus } from "lucide-react";
-import { useParams, useRouter } from "next/navigation";
-import { BillboardColumns, columns } from "./columns";
+import { OrderColumns, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
-import ApiList from "@/components/ui/api-list";
-type BillboardClientProps = {
-  data: BillboardColumns[];
+type OrderClientProps = {
+  data: OrderColumns[];
 };
-const BillboardClients: React.FC<BillboardClientProps> = ({ data }) => {
-  const router = useRouter();
-  const params = useParams();
+const OrderClients: React.FC<OrderClientProps> = ({ data }) => {
   return (
     <>
-      <div className="flex items-center justify-between">
         <Heading
-          title={`Billboards (${data?.length})`}
-          description="Manage your billboards"
+          title={`Orders (${data?.length})`}
+          description="Manage your orders"
         />
-        <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/5f898a0b69ca9a0ea56d0a11`)}
-        >
-          <Plus className="mr-2 h-4 w-4" />
-          Add new
-        </Button>
-      </div>
+
+
       <Separator />
-      <DataTable searchKey="label" columns={columns} data={data} />
-      <Separator />
-      <Heading title="Api" description="Api calls for billboard" />
-      <Separator />
-      <ApiList entityName="billboards" entityIdName="billboardId"/>
+      <DataTable searchKey="products" columns={columns} data={data} />
+
+ 
     </>
   );
 };
 
-export default BillboardClients;
+export default OrderClients;
