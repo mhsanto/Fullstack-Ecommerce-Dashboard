617f7b6db1f24541d8e19c1d3d3b823a98b0896b
Mahmodul Hasan <nicesanto92@gmail.com>
2023-11-12 19:35:25 +0600

    added theme switch option on navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/ModalProvider";
 
 import { Toaster } from "react-hot-toast";
+import { ThemeProvider } from "@/providers/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -21,9 +22,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ModalProvider />
-          {children}
-          <Toaster />
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <ModalProvider />
+            {children}
+            <Toaster />
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,6 +3,7 @@ import MainNav from "../main-nav";
 import StoreSwitcher from "../store-switcher";
 import { redirect } from "next/navigation";
 import prismaDB from "@/lib/prismaDB";
+import { ThemeToggle } from "../theme-toggle";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -19,6 +20,7 @@ const Navbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav />
         <div className="ml-auto flex  items-center space-x-4">
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
